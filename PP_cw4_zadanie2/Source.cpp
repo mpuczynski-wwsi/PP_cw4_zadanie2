ecf200cdc289c97c80d5a4e448987ef39c9e613a
Puczyński Mirosław <m_puczynski@orca.local>
2019-11-23 14:34:00 +0100

    001

--- PP_cw4_zadanie2/Source.cpp
+++ PP_cw4_zadanie2/Source.cpp
@@ -1,7 +1,26 @@
 #include<iostream>
 using namespace std;
 
+int zliczCyfry(long long a) {
+	int licznik_cyfr = 0;
+	if (a == 0) {
+		return licznik_cyfr;
+	}
+	else if (a < 0) {
+		a = -a;
+	}
+
+	while (a > 0) {
+		a = a / 10;
+		licznik_cyfr++;
+	}
+	return licznik_cyfr;
+}
 
 int main() {
+	long long  liczba;
+	cout << "Podaj liczbe: ";
+	cin >> liczba;
+	cout << "\n" << " ilosc cyfr liczby " << liczba << " wynosi " << zliczCyfry(liczba);
 
 }
